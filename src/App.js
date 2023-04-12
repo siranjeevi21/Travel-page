8a9e9b19a77a4a8fc084abd185e7f896ca34fb90
siranjeevi21 <jeevisiran21@gmail.com>
2023-04-12 11:12:25 +0530

    adding required files

---
+++ src/App.js
@@ -0,0 +1,158 @@
+// import React from 'react'
+
+
+// const App =() => {
+//     let color = "red"
+//     let a = 2323
+//     return (
+//         <div style={{backgroundColor:color}}>
+//             tha value of a is {a}
+//         </div>
+//     )
+
+// }
+// export default App;
+
+// import React from 'react'
+// import './style.css'
+// export const App = () => {
+//     let a = {
+//         backgroundColor:"red", 
+//         fontSize:"48px",
+//         border:"10px solid black"
+//     }
+//   return (
+//     <div style={a}//these are called as atribute
+//     >App</div>
+//   )
+// }
+// export default App;
+
+// import React from 'react'
+// import './style.css'
+// export const App = () => {
+
+//   return (
+//     <div className="box"
+//     >App</div>
+//   )
+// }
+// export default App;
+
+// import React from 'react'
+// import Main from './components/Main'
+// export const App = () => {
+//   return (
+//     <div className='box'>App
+//         <Main />
+//     </div>
+    
+//   )
+// }
+// export default App;
+
+
+// import React from 'react'
+// import style from './App.module.css'
+// export const App = () => {
+//     console.log("hello world")
+//   return (
+//     <div className={style.box}>App</div>
+//   )
+// }
+
+
+// import React from 'react'
+// import style from './App.module.css'
+// import Main from './components/Main'
+// export const App = () => {
+//   return (
+//     <div>App
+//         <Main />
+//     </div>
+
+//   )
+// }
+
+// export default App;
+
+// import React from 'react'
+// import Container from './components/Container'
+// export const App = () => {
+//   return (
+//     < Container/>
+//   )
+// }
+// export default App
+
+// import React from 'react'
+// import {data} from './data'
+// const App = () => {
+//   return (
+//     <div id='container'>
+//       {
+//         data.map((e, i) => (
+//           <div key={i} id='card' style={{background:i%2===0?"red":"blue"}}>
+//             <h1>Title:{e.title} </h1>
+//             {/* <h3>Username:{e.username}</h3>
+//             <p>Email{e.email}:</p> */}
+//           </div>
+//         ))
+//       }
+//     </div>
+//   )
+// }
+// export default App
+
+// import React from 'react'
+// import {photos} from './post'
+// const App = () => {
+//   return (
+//     <div id='container'>
+//       <div>
+//         <h1 id='head'>Tourist Places</h1>
+//         <div id='body'>
+//           {
+//             photos.map((e, i) => (
+//               <div key={i} id='card' >
+//                 <h3>{e.title} </h3>
+//                 <img src={e.url} alt="descriptive text"/>
+//               </div>
+//             ))
+//           }
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+// export default App
+
+// import React from 'react'
+
+// const App = () => {
+//   const a = true
+//   if (a){
+//     return (
+//       <div>App</div>
+//     )
+//   }
+  
+// }
+
+// export default App
+
+import React, {useState} from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+
+
+
+
---
+++ src/post.js
@@ -0,0 +1,51 @@
+export const photos = [
+    {
+      "albumId": 1,
+      "id": 1,
+      "title": "Ladakh",
+      "url": "https://imgs.search.brave.com/1lx6YdVwoX_VSSc-cqOe2hACu3SI_gOYsElHslwtTko/rs:fit:867:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5w/bmJpSE1RZWRPLUY2/RFBSQkt0VkZRSGFF/RCZwaWQ9QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 2,
+      "title": "Goa",
+      "url": "https://imgs.search.brave.com/GFIXXelPQAzw8E4v0yXZrdBXyxa-qe-rh7c6jtDS56w/rs:fit:759:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC54/VjFXYk9WVHdfT3ZO/TVpEOVhrRGt3SGFF/byZwaWQ9QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 3,
+      "title": "Chennai",
+      "url": "https://imgs.search.brave.com/boqcO2KyB6cttv9BMAJzIQrF9z33Ksj6B-r6dDD8JnU/rs:fit:756:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC45/aWJQcXh0NDNFcGV6/dXVicUZ3RmpnSGFF/cCZwaWQ9QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 4,
+      "title": "Banglore",
+      "url": "https://imgs.search.brave.com/QYQs5G2fUQIUwDM9J6QUitNo-twcjJk-I3wioKD3W9M/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4z/UEh3SThBVGwyaTFl/SGNuTkVxRDFnSGFF/OCZwaWQ9QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 5,
+      "title": "Agra",
+      "url": "https://imgs.search.brave.com/pQMriV9aZ2we5XrnebKpkETG4OCHTCd57mXZdxQHix8/rs:fit:722:225:1/g:ce/aHR0cHM6Ly90c2Ux/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC5oVEpIN01p/ZVNPUGFNbjhsemJY/X3FnSGFFMyZwaWQ9/QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 6,
+      "title": "Kerala",
+      "url": "https://imgs.search.brave.com/YZOiCmMr-T9EtbBNhU_hhtcVACZWi8U0t0JAY_TyFFI/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5E/RU14bVBYaV9helpw/X2dKRDlXSFh3SGFF/NyZwaWQ9QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 7,
+      "title": "Kolkata",
+      "url": "https://imgs.search.brave.com/foeLsknsc7dHdsQq5QnN-b5dH_cfDqyB7htXOGFWij0/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC50/QV9CWXlDV21YWENx/eXNlRDB4SDNBSGFF/OCZwaWQ9QXBp"
+    },
+    {
+      "albumId": 1,
+      "id": 8,
+      "title": "Hyderabad",
+      "url": "https://imgs.search.brave.com/Ihq8_lcyJGBHUeScOAg0UUKsvTsI3IRkZUbN9Oz0YDQ/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC43/UmdHOEU1OFBmZ0Iw/VVZVLVIxdFJ3SGFF/OCZwaWQ9QXBp"
+  
+    }]
+    
