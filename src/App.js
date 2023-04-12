8a229acb4fd00a9a9a2b7b7b5e38178da0acd871
siranjeevi21 <jeevisiran21@gmail.com>
2023-04-12 11:57:33 +0530

    eroireo

--- src/App.js
+++ src/App.js
@@ -141,18 +141,87 @@
 
 // export default App
 
-import React, {useState} from 'react'
+// import React, {useState} from 'react'
+// import Main from './Main'
+// const App = () => {
+//   let a = 10
+
+//   const [show, func] = useState(1)
+//   return (
+//     <div onClick={}>App {a}
+//       <Main random={""} />
+//     </div>
+    
+//   )
+// }
+
+// export default App
+
+
+// import { tab } from '@testing-library/user-event/dist/tab'
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const [tabs, setTabs] = useState("red")
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={() => setTabs("red")}>Red</button>
+//         <button onClick={() => setTabs("blue")}>Green</button>
+//         <button onClick={() => setTabs("green")}>Blue</button>
+//       </div>
+//       <div>
+//         {
+//           tabs=="red"
+//           ? <div style={{height:"90vh", backgroundColor:tabs}}></div>
+//           : tabs=="blue" 
+//           ? <div style={{height:"90vh", backgroundColor:tabs}}></div>
+//           :  <div style={{height:"90vh", backgroundColor:tabs}}></div>
+//         }
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from 'react'
 
 const App = () => {
+  const [button, inCreamnt] = useState(0)
+
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <h1>Count: {button}</h1>
+        <div>
+          {
+            button < 100
+              ? <button onClick={() => inCreamnt(button+10)}>increament</button>
+              : <button>increament</button>
+          }
+          {
+            button > 0
+              ? <button onClick={() => inCreamnt(button-10)}>decrement</button>
+              : <button>decrement</button>
+          }
+        </div>
+        {/* <button onClick={() => setTabs("blue")}>Green</button>
+        <button onClick={() => setTabs("green")}>Blue</button> */}
+      </div>
+      <div>
+        {
+          button<50
+          ? <div style={{height:"90vh", backgroundColor:"red"}}></div>
+          : button>50 &&  button<75
+          ? <div style={{height:"90vh", backgroundColor:"blue"}}></div>
+          :  <div style={{height:"90vh", backgroundColor:"green"}}></div>
+        }
+      </div>
+    </div>
   )
 }
 
 export default App
-
-
-
-
-
-
